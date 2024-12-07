4149d99717b1700ef3a74a6860afc51092bceed0
Jonathon Anderson <coding@jpanderson.dev>
2024-12-07 14:04:38 -0800

    Minor extra test for part 2 day 7

--- test/advent_of_code/day_07_test.exs
+++ test/advent_of_code/day_07_test.exs
@@ -48,4 +48,24 @@ defmodule AdventOfCode.Day07Test do
 
     assert 11387 == result
   end
+
+  @tag :day7
+  test "part2 check for all concat" do
+    input = """
+    190: 10 19
+    3267: 81 40 27
+    83: 17 5
+    156: 15 6
+    7290: 6 8 6 15
+    161011: 16 10 13
+    192: 17 8 14
+    21037: 9 7 18 13
+    292: 11 6 16 20
+    4455: 44 5 5
+    """
+
+    result = part2(input)
+
+    assert 15842 == result
+  end
 end

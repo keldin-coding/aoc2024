226de6c4a5238be9d2ac0a38078bab2971013946
Jonathon Anderson <coding@jpanderson.dev>
2024-12-07 13:39:35 -0800

    Day 7 complete without combinatorics

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,7 +1,112 @@
 defmodule AdventOfCode.Day07 do
-  def part1(_args) do
+  # 873610404290 is too low
+  def part1(input) do
+    input
+    |> create_values_map()
+    |> Enum.map(fn {target, [first | operands]} ->
+      Task.async(fn ->
+        results =
+          [calculate(operands, target, :plus, first), calculate(operands, target, :mult, first)]
+          |> List.flatten()
+
+        {target, results}
+      end)
+    end)
+    |> Task.await_many()
+    |> Enum.filter(fn {target, results} -> Enum.any?(results, &(&1 == target)) end)
+    |> Enum.reduce(0, fn {v, _}, acc -> acc + v end)
   end
 
-  def part2(_args) do
+  # 29229114137295 too low
+  def part2(input) do
+    input
+    |> create_values_map()
+    |> Enum.map(fn {target, [first | operands]} ->
+      Task.async(fn ->
+        results =
+          [
+            calculate_with_concat(operands, target, :plus, first),
+            calculate_with_concat(operands, target, :mult, first),
+            calculate_with_concat(operands, target, :concat, first)
+          ]
+          |> List.flatten()
+
+        {target, results}
+      end)
+    end)
+    |> Task.await_many()
+    |> Enum.filter(fn {target, results} -> Enum.any?(results, &(&1 == target)) end)
+    |> Enum.reduce(0, fn {v, _}, acc -> acc + v end)
+  end
+
+  # End of list, return result. Really shouldn't get here but.
+  defp calculate(_, target, _, acc) when acc > target, do: -1
+
+  defp calculate([last], _, :plus, acc), do: acc + last
+  defp calculate([last], _, :mult, acc), do: acc * last
+
+  defp calculate([current | operands], target, :plus, acc) do
+    acc = acc + current
+    [calculate(operands, target, :plus, acc), calculate(operands, target, :mult, acc)]
+  end
+
+  defp calculate([current | operands], target, :mult, acc) do
+    acc = acc * current
+    [calculate(operands, target, :plus, acc), calculate(operands, target, :mult, acc)]
+  end
+
+  # We failed, it's already too big
+  defp calculate_with_concat(_, target, _, acc) when acc > target, do: -1
+
+  # Last item
+  defp calculate_with_concat([last], _, :plus, acc), do: acc + last
+  defp calculate_with_concat([last], _, :mult, acc), do: acc * last
+  defp calculate_with_concat([last], _, :concat, acc), do: concatenate_digits(acc, last)
+
+  defp calculate_with_concat([current | operands], target, :plus, acc) do
+    acc = acc + current
+
+    [
+      calculate_with_concat(operands, target, :plus, acc),
+      calculate_with_concat(operands, target, :mult, acc),
+      calculate_with_concat(operands, target, :concat, acc)
+    ]
+  end
+
+  defp calculate_with_concat([current | operands], target, :mult, acc) do
+    acc = acc * current
+
+    [
+      calculate_with_concat(operands, target, :plus, acc),
+      calculate_with_concat(operands, target, :mult, acc),
+      calculate_with_concat(operands, target, :concat, acc)
+    ]
+  end
+
+  defp calculate_with_concat([current | operands], target, :concat, acc) do
+    acc = concatenate_digits(acc, current)
+
+    [
+      calculate_with_concat(operands, target, :plus, acc),
+      calculate_with_concat(operands, target, :mult, acc),
+      calculate_with_concat(operands, target, :concat, acc)
+    ]
+  end
+
+  defp concatenate_digits(a, b) do
+    "#{Integer.to_string(a)}#{Integer.to_string(b)}" |> String.to_integer()
+  end
+
+  defp create_values_map(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(%{}, fn line, acc ->
+      [test_value, numbers] = String.split(line, ": ")
+      test_value = String.to_integer(test_value)
+
+      numbers = numbers |> String.split(" ") |> Enum.map(&String.to_integer/1)
+
+      Map.put(acc, test_value, numbers)
+    end)
   end
 end
--- test/advent_of_code/day_07_test.exs
+++ test/advent_of_code/day_07_test.exs
@@ -3,19 +3,49 @@ defmodule AdventOfCode.Day07Test do
 
   import AdventOfCode.Day07
 
-  @tag :skip
+  @tag :day7
   test "part1" do
-    input = nil
+    input = """
+    190: 10 19
+    3267: 81 40 27
+    83: 17 5
+    156: 15 6
+    7290: 6 8 6 15
+    161011: 16 10 13
+    192: 17 8 14
+    21037: 9 7 18 13
+    292: 11 6 16 20
+    """
+
+    # input = "190: 10 19"
+
     result = part1(input)
 
-    assert result
+    assert 3749 == result
   end
 
-  @tag :skip
+  @tag :day7_small
+  test "part1 small samples" do
+    assert 2 == part1("2: 1 1")
+    assert 3 == part1("3: 1 2 1")
+  end
+
+  @tag :day7
   test "part2" do
-    input = nil
+    input = """
+    190: 10 19
+    3267: 81 40 27
+    83: 17 5
+    156: 15 6
+    7290: 6 8 6 15
+    161011: 16 10 13
+    192: 17 8 14
+    21037: 9 7 18 13
+    292: 11 6 16 20
+    """
+
     result = part2(input)
 
-    assert result
+    assert 11387 == result
   end
 end

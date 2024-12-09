77b5faadc681500ff0726b5ef1188826039d828f
Jonathon Anderson <coding@jpanderson.dev>
2024-12-08 23:10:27 -0800

    Part 1

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -1,7 +1,94 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  # The puzzle uses `.` to indicate free space, we're using -1
+
+  def part1(input) do
+    input
+    |> split_input()
+    |> create_fileblocks()
+    |> defragment()
+    |> IO.inspect()
+    |> Enum.with_index()
+    |> Enum.reduce(0, fn {val, index}, acc -> if val == -1, do: acc, else: acc + val * index end)
   end
 
   def part2(_args) do
+  end
+
+  defp inspect_end_of_list(list) do
+    list
+    |> Enum.reverse()
+    |> Enum.reverse()
+  end
+
+  def defragment(fileblock_data) do
+    # number_of_free_slots = Enum.count(fileblock_data, &(&1 == -1))
+    fb_len = length(fileblock_data)
+
+    defragment(
+      Enum.with_index(fileblock_data),
+      Enum.with_index(Enum.reverse(fileblock_data)),
+      fb_len,
+      []
+    )
+  end
+
+  # We think we've moved everything, just tack the rest of the existing data onto the end
+  defp defragment([{_, c_index} | _], [{last, last_index} | _], total_data, defragmented)
+       when c_index + last_index >= total_data do
+    if last != -1, do: Enum.reverse(defragmented), else: Enum.reverse([last | defragmented])
+  end
+
+  # defp defragment([{c, c_index} | _], [{_, last_index} | _], total_data, defragmented)
+  #      when c_index + last_index + 1 > total_data do
+  #   Enum.reverse([c | defragmented])
+  # end
+
+  # Current slot is data
+  defp defragment([{c, _} | ordered_data], backwards, total_data, defragmented) when c != -1,
+    do: defragment(ordered_data, backwards, total_data, [c | defragmented])
+
+  # Current slot is free and last slot is data
+  defp defragment([{c, _} | ordered_data], [{last, _} | backwards], total_data, defragmented)
+       when c == -1 and last != -1 do
+    defragment(ordered_data, backwards, total_data, [last | defragmented])
+  end
+
+  # Current slot is free and last slot is free
+  defp defragment(
+         [{c, c_index} | ordered_data],
+         [{last, _} | backwards],
+         total_data,
+         defragmented
+       )
+       when c == -1 and last == -1 do
+    defragment([{c, c_index} | ordered_data], backwards, total_data, defragmented)
+  end
+
+  def create_fileblocks(diskmap), do: create_fileblocks(diskmap, :file, [])
+
+  defp create_fileblocks([], _, file_data), do: Enum.reverse(file_data)
+
+  defp create_fileblocks([{current_num, current_id} | rest], :file, file_data) do
+    create_fileblocks(
+      rest,
+      :free,
+      List.duplicate(Integer.floor_div(current_id, 2), current_num) ++ file_data
+    )
+  end
+
+  defp create_fileblocks([{current_num, _} | rest], :free, file_data) do
+    create_fileblocks(
+      rest,
+      :file,
+      List.duplicate(-1, current_num) ++ file_data
+    )
+  end
+
+  def split_input(input) do
+    input
+    |> String.trim()
+    |> String.split("", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.with_index()
   end
 end
--- test/advent_of_code/day_09_test.exs
+++ test/advent_of_code/day_09_test.exs
@@ -3,15 +3,63 @@ defmodule AdventOfCode.Day09Test do
 
   import AdventOfCode.Day09
 
-  @tag :skip
+  @tag :day9
   test "part1" do
-    input = nil
+    input = "2333133121414131402"
     result = part1(input)
 
-    assert result
+    assert 1928 == result
   end
 
-  @tag :skip
+  @tag :day9
+  test "create_fileblocks/1" do
+    input = "2333133121414131402"
+
+    result = create_fileblocks(split_input(input))
+
+    result_as_str =
+      result
+      |> Enum.map(fn i -> if i == -1, do: ".", else: "#{i}" end)
+      |> Enum.join("")
+
+    assert "00...111...2...333.44.5555.6666.777.888899" == result_as_str
+  end
+
+  # Temp test
+  @tag :day9
+  test "defragment" do
+    input = "12345"
+
+    result =
+      input
+      |> split_input()
+      |> create_fileblocks()
+      |> defragment()
+
+    result_as_str =
+      result
+      |> Enum.map(fn i -> if i == -1, do: ".", else: "#{i}" end)
+      |> Enum.join("")
+
+    assert "022111222." == result_as_str
+
+    input = "2333133121414131402"
+
+    result =
+      input
+      |> split_input()
+      |> create_fileblocks()
+      |> defragment()
+
+    result_as_str =
+      result
+      |> Enum.map(fn i -> if i == -1, do: ".", else: "#{i}" end)
+      |> Enum.join("")
+
+    assert "0099811188827773336446555566" == result_as_str
+  end
+
+  @tag :day9
   test "part2" do
     input = nil
     result = part2(input)

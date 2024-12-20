238fa038097b90a6f00b329274840e67c598a950
Jonathon Anderson <coding@jpanderson.dev>
2024-12-20 12:10:23 -0800

    It aint done

--- lib/advent_of_code/day_12.ex
+++ lib/advent_of_code/day_12.ex
@@ -1,7 +1,60 @@
 defmodule AdventOfCode.Day12 do
-  def part1(_args) do
+  import Coordinates
+
+  def part1(input) do
+    grid_data =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.split(&1, "", trim: true))
+      |> to_grid()
+
+    {regions, _} = contiguous_regions(grid_data)
+
+    regions
+    |> Enum.map(fn region -> {calculate_area(region), calculate_perimeter(region)} end)
   end
 
-  def part2(_args) do
+  def part2(input) do
+  end
+
+  defp calculate_area(region), do: length(region)
+
+  defp calculate_perimter([_]), do: 4
+
+  defp calculate_perimeter(region) do
+  end
+
+  defp contiguous_regions(grid_data) do
+    grid = Map.get(grid_data, :grid)
+
+    Map.keys(grid)
+    |> Enum.reduce({[], %{}}, fn point, acc -> contiguous_regions(grid, point, acc) end)
+  end
+
+  defp contiguous_regions(_, point, {_, already_seen} = acc) when is_map_key(already_seen, point),
+    do: acc
+
+  defp contiguous_regions(grid, point, {regions, already_seen}) do
+    plant = Map.get(grid, point)
+
+    current_region = contiguous_regions(grid, point, plant, [])
+
+    already_seen =
+      Enum.reduce(current_region, already_seen, fn i, acc -> Map.put(acc, i, true) end)
+
+    {[current_region | regions], already_seen}
+  end
+
+  defp contiguous_regions(grid, point, _, acc) when not is_map_key(grid, point), do: acc
+
+  defp contiguous_regions(grid, point, plant, acc) do
+    if Map.get(grid, point) == plant and !Enum.member?(acc, point) do
+      acc = [point | acc]
+
+      [move_up(point), move_right(point), move_down(point), move_left(point)]
+      |> Enum.reduce(acc, fn p, acc -> contiguous_regions(grid, p, plant, acc) end)
+    else
+      acc
+    end
   end
 end
--- test/advent_of_code/day_12_test.exs
+++ test/advent_of_code/day_12_test.exs
@@ -3,15 +3,45 @@ defmodule AdventOfCode.Day12Test do
 
   import AdventOfCode.Day12
 
-  @tag :skip
+  @tag :day12
+  @tag timeout: :infinity
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = """
+    AAAA
+    BBCD
+    BBCC
+    EEEC
+    """
 
-    assert result
+    assert 140 == part1(input)
+
+    input = """
+    OOOOO
+    OXOXO
+    OOOOO
+    OXOXO
+    OOOOO
+    """
+
+    assert 772 == part1(input)
+
+    input = """
+    RRRRIICCFF
+    RRRRIICCCF
+    VVRRRCCFFF
+    VVRCCCJFFF
+    VVVVCJJCFE
+    VVIVCCJJEE
+    VVIIICJJEE
+    MIIIIIJJEE
+    MIIISIJEEE
+    MMMISSJEEE
+    """
+
+    assert 1930 == part1(input)
   end
 
-  @tag :skip
+  @tag :day12
   test "part2" do
     input = nil
     result = part2(input)
